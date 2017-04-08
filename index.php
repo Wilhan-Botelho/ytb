c1d7acaf7fcf82d785555d872f95d4977b97fa15
Wilhan Botelho <Wilhan Botelho>
2017-04-08 12:47:25 -0300

    Adicionado suporte à telas menores

--- index.php
+++ index.php
@@ -2,25 +2,36 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <title>Baixe vídeos públicos do YouTube</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen" />
-        <link href="css/custom.css" rel="stylesheet" media="screen" />
+        <meta id="mvp" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+        <link href="css/custom.css" rel="stylesheet" media="screen"/>
     </head>
     <body>
-        <form class="form-download" method="post" id="download" action="lista.php">
-            <h1 class="form-download-heading">Download do Youtube</h1>
-            <input type="text" name="videoid" placeholder="Link do YoutTube ou Id do Vídeo" />
-            <input type="text" name="user" autocomplete="off" placeholder="Usuário" />
-            <input type="password" name="pass" placeholder="Senha" />
-            <div class="text-center">
-                <input class="btn btn-primary" type="submit" value="Buscar opções" />
+        <div class="container">
+            <div class="row">
+                <form class="form-download" method="post" id="download" action="lista.php">
+                    <h1 class="form-download-heading">Download do Youtube</h1>
+                    <input type="text" name="videoid" placeholder="Link do YoutTube ou Id do Vídeo" />
+                    <input type="text" name="user" autocomplete="off" placeholder="Usuário" />
+                    <input type="password" name="pass" placeholder="Senha" />
+                    <div class="text-center">
+                        <input class="btn btn-primary" type="submit" value="Buscar opções" />
+                    </div>
+                    <p>Exemplos de IDs válidos:</p>
+                    <ul>
+                        <li>youtube.com/watch?v=<b>aBCdEFGhIJk</b></li>
+                        <li><b>youtube.com/watch?v=aBCdEFGhIJk</b></li>
+                        <li><b>youtu.be/aBCdEFGhIJk</b></li>
+                        <li><b>m.youtube.com/watch?v=aBCdEFGhIJk</b></li>
+                    </ul>
+                </form>
             </div>
-            <p>Exemplos de IDs válidos:</p>
-            <ul>
-                <li>youtube.com/watch?v=<b>aBCdEFGhIJk</b></li>
-                <li><b>youtube.com/watch?v=aBCdEFGhIJk</b></li>
-                <li><b>youtu.be/aBCdEFGhIJk</b></li>
-                <li><b>m.youtube.com/watch?v=aBCdEFGhIJk</b></li>
-            </ul>
-        </form>
+        </div>
     </body>
+    <script>
+        window.onload = function () {
+            var mvp = document.getElementById('mvp');
+            mvp.setAttribute('content','width=' + window.innerWidth );
+        }
+    </script> 
 </html>
--- lista.php
+++ lista.php
@@ -19,27 +19,31 @@
             $video_id = $_POST['videoid'];
             $arrRes = $yt->getDownloadLinks($video_id);
         ?>
-        <div class="form-download">
-            <h1 class="form-download-heading">Resultados</h1>
-            <div id="info">
-                <img src="<?php echo $arrRes['thumbnail_url'] ?>" border="0" hspace="2" vspace="2" />
-                <p><?php echo $arrRes['title'] ?></p>
-            </div>
-            <p align="center">Formatos disponíveis:</p>
-            <ul>
-                <?php foreach ($arrRes['links'] as $key => $value) {
-                    echo '<li>';
-                    echo '<a title="download" href="download.php?' .
-                            'mime=' . $value['type'] .
-                            '&title=' . urlencode($arrRes['title']) .
-                            '&size=' . $value['size'] .
-                            '&token=' . base64_encode(urlencode($value['url'])) . '">' .
-                        $value['type'] . ' | ' . $value['format'] . '</a> => ' . $value['formatedSize'];
-                    echo '</li>';
-                } ?>
-            </ul>
-            <div class="row text-center">
-                <a class="btn btn-primary" href="/ytb">Voltar</a>
+        <div class="container">
+            <div class="row">
+                <div class="form-download">
+                    <h1 class="form-download-heading">Resultados</h1>
+                    <div id="info">
+                        <img src="<?php echo $arrRes['thumbnail_url'] ?>" border="0" hspace="2" vspace="2" />
+                        <p><?php echo $arrRes['title'] ?></p>
+                    </div>
+                    <p align="center">Formatos disponíveis:</p>
+                    <ul>
+                        <?php foreach ($arrRes['links'] as $key => $value) {
+                            echo '<li>';
+                            echo '<a title="download" href="download.php?' .
+                                    'mime=' . $value['type'] .
+                                    '&title=' . urlencode($arrRes['title']) .
+                                    '&size=' . $value['size'] .
+                                    '&token=' . base64_encode(urlencode($value['url'])) . '">' .
+                                $value['type'] . ' | ' . $value['format'] . '</a> => ' . $value['formatedSize'];
+                            echo '</li>';
+                        } ?>
+                    </ul>
+                    <div class="row text-center">
+                        <a class="btn btn-primary" href="/ytb">Voltar</a>
+                    </div>
+                </div>
             </div>
     </body>
 </html>
